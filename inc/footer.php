6129c672ed7bb7c2253673ea60ce5414de1fa3ef
Muhammad Amran Bhuiyan <35559798+muharib979@users.noreply.github.com>
2018-01-18 22:01:59 +0600

    file

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+<div class="well">
+			<h3>Website:www.amran979@gmail.com
+			<span class="pull-right">Like Us:www.facebook.com/amranbhuiyan</span>
+			</h3>
+			
+		</div>			
+		</div>
+		
+	</body>
+</html>
